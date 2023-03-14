befc5da5db8e43273fd85a293a39362a18c863bd
raveena <raveenar.kale@gmail.com>
2023-03-15 01:49:52 +0530

    updating routing features into it

--- effectpractice/src/App.js
+++ effectpractice/src/App.js
@@ -1,40 +1,30 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import React from "react";
+import Home from "./component/Home";
 import Header from "./component/Header";
+import About from "./component/About";
+import Contact from "./component/Contact";
+import Error from "./component/Error";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  const [state, setState] = useState(2);
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    // window.alert("useeffect");
-    async function getData() {
-      const get = await fetch(
-        `https://hub.dummyapis.com/employee?noofRecords=${state}&idStarts=1001`
-      );
-
-      const res = await get.json();
-      setData(res);
-    }
-    getData();
-
-    document.title = `(${state}) Employees online`;
-  }, [state]);
-
   return (
     <div>
       <Header />
+      <Home />
+      <Routes>
+        {/* Home */}
+        <Route path="/" element={<Home />} />
+
+        {/* About */}
+        <Route path="/about" element={<About />} />
+
+        {/* Contact */}
+        <Route path="/contact" element={<Contact />} />
+
+        <Route path="*" element={<Error />} />
+      </Routes>
       {/* {console.log("jsx")} */}
-      <button onClick={() => setState(state + 2)}>Click me{state}</button>
-      {data.map((element, index) => {
-        return (
-          <div className="data" key={index}>
-            <h4>{element.firstName}</h4>
-            <h4>{element.lastName}</h4>
-            <h4>{element.email}</h4>
-          </div>
-        );
-      })}
     </div>
   );
 }
--- effectpractice/src/component/Header.js
+++ effectpractice/src/component/Header.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import {Link} from "react-router-dom" 
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <Link to={"/"}><h1>Home</h1></Link>
+      <Link to={"/about"}> <h1>About</h1></Link>
+      <Link to={"/contact"}> <h1>Contact</h1></Link>
+    </div>
   )
 }
 
